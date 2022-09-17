b37c8091c84661aaf6ca46c51f88ec21b007a754
hbashift <chenpavel31@gmail.com>
2022-09-17 16:13:07 +0300

    minor fixes

--- src/Main.java
+++ src/Main.java
@@ -3,23 +3,23 @@ public class Main {
         Manager manager = new Manager();
 
         Task task1 = new Task("Таска1", "Обычная таска", Status.NEW);
-        int task1Id = manager.addNewTask(task1);
+        final int task1Id = manager.addNewTask(task1);
         Task task2 = new Task("Таска2", "Очень обычная таска", Status.IN_PROGRESS);
-        int task2Id = manager.addNewTask(task2);
+        final int task2Id = manager.addNewTask(task2);
 
         Epic epic1 = new Epic("Эпик1", "Эпик");
-        int epic1Id = manager.addNewEpic(epic1);
+        final int epic1Id = manager.addNewEpic(epic1);
 
         Subtask subtask1 = new Subtask("Сабтаска 1", "Обычная сабтаска", Status.NEW, epic1);
-        int subtask1Id = manager.addNewSubtask(subtask1);
+        final int subtask1Id = manager.addNewSubtask(subtask1);
         Subtask subtask2 = new Subtask("Сабтаска 2", "Очень обычная сабтаска", Status.IN_PROGRESS, epic1);
-        int subtask2Id = manager.addNewSubtask(subtask2);
+        final int subtask2Id = manager.addNewSubtask(subtask2);
 
         Epic epic2 = new Epic("Эпик2", "Эпик2");
-        int epic2Id = manager.addNewEpic(epic2);
+        final int epic2Id = manager.addNewEpic(epic2);
 
         Subtask subtask3 = new Subtask("Сабтаска3", "Прям обычная сабтаска", Status.NEW, epic2);
-        int subtask3Id = manager.addNewSubtask(subtask3);
+        final int subtask3Id = manager.addNewSubtask(subtask3);
 
         System.out.println("До изменения статуса");
         System.out.println(manager.getTasks());
